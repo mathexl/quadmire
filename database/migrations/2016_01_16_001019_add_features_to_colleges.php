777b0815cbe3de8e504212d10674cbe80adc38ed
Mathew <mdp2163@columbia.edu>
2016-01-16 02:25:34 -0500

    FIX

--- database/migrations/2016_01_16_001019_add_features_to_colleges.php
+++ database/migrations/2016_01_16_001019_add_features_to_colleges.php
@@ -2,31 +2,3 @@
 
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
-
-class AddFeatureasdsToCollegesxx extends Migration
-{
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
-        Schema::table('colleges', function (Blueprint $table) {
-            //
-
-        });
-    }
-
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        Schema::table('colleges', function (Blueprint $table) {
-            //
-        });
-    }
-}
